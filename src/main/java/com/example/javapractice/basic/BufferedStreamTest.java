8967efaa4ed7df208104e945f3c22b25165e25b5
wun-yu-lin <76545619+wun-yu-lin@users.noreply.github.com>
2024-04-19 16:59:42 +0800

    BufferedStream

---
+++ src/main/java/com/example/javapractice/basic/BufferedStreamTest.java
@@ -0,0 +1,17 @@
+package com.example.javapractice.basic;
+
+import java.io.*;
+
+public class BufferedStreamTest {
+
+    public static void dump(InputStream src, OutputStream dest) throws IOException {
+        try (BufferedInputStream input = new BufferedInputStream(src);
+             BufferedOutputStream output = new BufferedOutputStream(dest)) {
+            byte[] data = new byte[1024]; //as buffer
+            int length = 0;
+            while ((length = input.read(data)) != -1) {
+                output.write(data, 0, length);
+            }
+        }
+    }
+}
