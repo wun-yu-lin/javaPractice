9e3595850fbfd607987809ac00b122041d91a124
wun-yu-lin <76545619+wun-yu-lin@users.noreply.github.com>
2024-05-05 21:59:19 +0800

    test

--- src/main/java/com/example/javapractice/test.java
+++ src/main/java/com/example/javapractice/test.java
@@ -38,6 +38,7 @@ public class test {
         Collections.sort(accounts);
         accounts.forEach(System.out::println);
         HashMap<String, Integer> map = new HashMap<>();
+        map.put("John", 100);
         //
         //test commit
     }
